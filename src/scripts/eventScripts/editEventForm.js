3272c3808d8c0d853fb3b5caca4b2d7f0a65f8d6
Chris Morgan <bluemorgan2323@gmail.com>
2019-04-04 17:33:13 -0500

    No changes in functionality, just added classes to elements and linked bootstrap in index.html.

--- src/scripts/eventScripts/editEventForm.js
+++ src/scripts/eventScripts/editEventForm.js
@@ -10,27 +10,43 @@ const editEventForm = {
         let userID = sessionStorage.getItem("userID");
         const eventSection = document.querySelector("#events-section");
         const editEventCard = HTMLFactory.createElementWithText("div", undefined, "editEvent-card");
+        editEventCard.classList.add("card");
         const editEventHeader = editEventCard.appendChild(HTMLFactory.createElementWithText("h3", "Edit Event Information", "editEvent-header"));
+        editEventHeader.classList.add("card-header");
         const editEventForm = editEventCard.appendChild(HTMLFactory.createElementWithText("form", undefined, "editEvent-form"));
         const editNameDiv = editEventForm.appendChild(HTMLFactory.createElementWithText("div"))
+        editNameDiv.classList.add("form-group");
         const editEventNameLabel = editNameDiv.appendChild(HTMLFactory.createElementWithText("label", "Event Name:"));
         const editEventNameInput = editNameDiv.appendChild(HTMLFactory.createElementWithText("input", undefined, "editEvent-nameInput"));
+        editEventNameInput.classList.add("form-control");
         editEventNameInput.value = name;
         const editDateDiv = editEventForm.appendChild(HTMLFactory.createElementWithText("div"))
+        editDateDiv.classList.add("form-group");
         const editEventDateLabel = editDateDiv.appendChild(HTMLFactory.createElementWithText("label", "Event Date:"));
         const editEventDateInput = editDateDiv.appendChild(HTMLFactory.createElementWithText("input", undefined, "editEvent-dateInput"));
+        editEventDateInput.classList.add("form-control");
         editEventDateInput.value = date;
         const editLocationDiv = editEventForm.appendChild(HTMLFactory.createElementWithText("div"))
+        editLocationDiv.classList.add("form-group");
         const editEventLocationLabel = editLocationDiv.appendChild(HTMLFactory.createElementWithText("label", "Event Location:"));
         const editEventLocationInput = editLocationDiv.appendChild(HTMLFactory.createElementWithText("input", location, "editEvent-locationInput"));
+        editEventLocationInput.classList.add("form-control");
         editEventLocationInput.value = location;
-        const submitEditButton = editEventForm.appendChild(HTMLFactory.createElementWithText("button", "Submit", "editForm-submitButton"));
+        const buttonGroup = editEventForm.appendChild(HTMLFactory.createElementWithText("div", undefined, "editForm-buttonGroup"));
+        buttonGroup.classList.add("btn-group");
+        const submitEditButton = buttonGroup.appendChild(HTMLFactory.createElementWithText("button", "Submit", "editForm-submitButton"));
+        submitEditButton.classList.add("btn");
+        submitEditButton.classList.add("btn-info");
+        submitEditButton.classList.add("btn-sm");
         submitEditButton.type = "button";
         submitEditButton.addEventListener("click", function(){
             handlersForEvents.submitEditEvent(id);
         })
-        const cancelEditButton = editEventForm.appendChild(HTMLFactory.createElementWithText("button", "Cancel", "editForm-cancelButton"));
+        const cancelEditButton = buttonGroup.appendChild(HTMLFactory.createElementWithText("button", "Cancel", "editForm-cancelButton"));
         cancelEditButton.type = "button";
+        cancelEditButton.classList.add("btn");
+        cancelEditButton.classList.add("btn-danger");
+        cancelEditButton.classList.add("btn-sm");
         cancelEditButton.addEventListener("click", function(){
             HTMLFactory.clearContainer(eventSection);
             API.getEvents(userID).then(response => eventHTML.listEventsToDom(response));
--- src/scripts/eventScripts/eventHTML.js
+++ src/scripts/eventScripts/eventHTML.js
@@ -5,7 +5,9 @@ import editEventForm from "./editEventForm"
 const eventHTML = {
     buildEventCard: () => {
         const eventCard = HTMLFactory.createElementWithText("div", undefined, "eventCard-card");
+        eventCard.classList.add("card");
         const cardHeader = eventCard.appendChild(HTMLFactory.createElementWithText("h3", "Events", "eventCard-header"))
+        cardHeader.classList.add("card-header")
 
         return eventCard;
     },
@@ -13,11 +15,19 @@ const eventHTML = {
         const eventSection = document.querySelector("#events-section");
         const eventCard = document.querySelector("#eventCard-card");
         const cardBody = eventCard.appendChild(HTMLFactory.createElementWithText("div", undefined, `eventCard-body--${eventObj.id}`));
-        const eventName = cardBody.appendChild(HTMLFactory.createElementWithText("p", eventObj.eventName, `eventCard-name--${eventObj.id}`));
+        cardBody.classList.add("card-body");
+        const eventName = cardBody.appendChild(HTMLFactory.createElementWithText("h3", eventObj.eventName, `eventCard-name--${eventObj.id}`));
+        eventName.classList.add("card-text");
         const eventDate = cardBody.appendChild(HTMLFactory.createElementWithText("p", eventObj.eventDate, `eventCard-date--${eventObj.id}`));
+        eventDate.classList.add("card-text");
         const eventLocation = cardBody.appendChild(HTMLFactory.createElementWithText("p", eventObj.eventLocation, `eventCard-location--${eventObj.id}`));
+        eventLocation.classList.add("card-text");
         const eventButtonGroup = cardBody.appendChild(HTMLFactory.createElementWithText("div", undefined, `eventCard-buttonGroup--${eventObj.id}`));
+        eventButtonGroup.classList.add("btn-group");
         const eventEditButton = eventButtonGroup.appendChild(HTMLFactory.createElementWithText("button", "Edit Event", `eventCard-editButton--${eventObj.id}`));
+        eventEditButton.classList.add("btn");
+        eventEditButton.classList.add("btn-sm");
+        eventEditButton.classList.add("btn-info");
         eventEditButton.addEventListener("click", function(){
             const nameToCopy = event.target.parentNode.parentNode.firstChild;
             const dateToCopy = nameToCopy.nextSibling;
@@ -28,6 +38,9 @@ const eventHTML = {
             eventSection.appendChild(editEventForm.editEventFormBuilder(nameToCopy.textContent, dateToCopy.textContent, locationToCopy.textContent, id));
         });
         const eventDeleteButton = eventButtonGroup.appendChild(HTMLFactory.createElementWithText("button", "Delete Event", `eventCard-deleteButton--${eventObj.id}`));
+        eventDeleteButton.classList.add("btn");
+        eventDeleteButton.classList.add("btn-sm");
+        eventDeleteButton.classList.add("btn-danger");
         eventDeleteButton.addEventListener("click", handlersForEvents.deleteEvent);
     },
     listEventsToDom: (eventsArray) => {
@@ -38,7 +51,11 @@ const eventHTML = {
         })
         const eventCard = document.querySelector("#eventCard-card");
         const cardFooter = eventCard.appendChild(HTMLFactory.createElementWithText("div", undefined, "eventCard-footer"));
+        cardFooter.classList.add("card-footer");
         const newEventButton = cardFooter.appendChild(HTMLFactory.createElementWithText("button", "Create New Event", "eventCard-createButton"));
+        newEventButton.classList.add("btn");
+        newEventButton.classList.add("btn-block");
+        newEventButton.classList.add("btn-primary");
         newEventButton.addEventListener("click", handlersForEvents.createNewEventHandler);
         return eventCard;
     }
