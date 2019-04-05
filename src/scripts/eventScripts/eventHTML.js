aa221c859b562a0d2d2e9b0c64705a8dae79a38f
Chris Morgan <bluemorgan2323@gmail.com>
2019-04-05 11:11:21 -0500

    Events that are the current user will be displayed with buttons, and have a class for their continer myEvent. The friends events will post below without buttons and with class friendEvent.

--- src/scripts/eventScripts/eventHTML.js
+++ src/scripts/eventScripts/eventHTML.js
@@ -9,32 +9,35 @@ const eventHTML = {
         eventCard.classList.add("card");
         const cardHeader = eventCard.appendChild(HTMLFactory.createElementWithText("h3", "Events", "eventCard-header"))
         cardHeader.classList.add("card-header")
+        const cardBody = eventCard.appendChild(HTMLFactory.createElementWithText("div", undefined, "eventCard-body"));
+        cardBody.classList.add("card-body");
 
         return eventCard;
     },
     // htmlforEachEvent takes the event object and creates the html structure for it
     HTMLforEachEvent: (eventObj) => {
         const eventSection = document.querySelector("#events-section");
-        const eventCard = document.querySelector("#eventCard-card");
-        const cardBody = eventCard.appendChild(HTMLFactory.createElementWithText("div", undefined, `eventCard-body--${eventObj.id}`));
-        cardBody.classList.add("card-body");
-        const eventName = cardBody.appendChild(HTMLFactory.createElementWithText("h3", eventObj.eventName, `eventCard-name--${eventObj.id}`));
+        const cardBody = document.querySelector("#eventCard-body");
+        const userID = sessionStorage.getItem("userID");
+        const eachEventContainer = cardBody.appendChild(HTMLFactory.createElementWithText("div", undefined, `event--${eventObj.id}`));
+        const eventName = eachEventContainer.appendChild(HTMLFactory.createElementWithText("h3", eventObj.eventName, `eventCard-name--${eventObj.id}`));
         eventName.classList.add("card-text");
-        const eventDate = cardBody.appendChild(HTMLFactory.createElementWithText("p", eventObj.eventDate, `eventCard-date--${eventObj.id}`));
+        const eventDate = eachEventContainer.appendChild(HTMLFactory.createElementWithText("p", eventObj.eventDate, `eventCard-date--${eventObj.id}`));
         eventDate.classList.add("card-text");
-        const eventLocation = cardBody.appendChild(HTMLFactory.createElementWithText("p", eventObj.eventLocation, `eventCard-location--${eventObj.id}`));
+        const eventLocation = eachEventContainer.appendChild(HTMLFactory.createElementWithText("p", eventObj.eventLocation, `eventCard-location--${eventObj.id}`));
         eventLocation.classList.add("card-text");
-        const eventButtonGroup = cardBody.appendChild(HTMLFactory.createElementWithText("div", undefined, `eventCard-buttonGroup--${eventObj.id}`));
-        eventButtonGroup.classList.add("btn-group");
-        const eventEditButton = eventButtonGroup.appendChild(HTMLFactory.createElementWithText("button", "Edit Event", `eventCard-editButton--${eventObj.id}`));
-        eventEditButton.classList.add("btn");
-        eventEditButton.classList.add("btn-sm");
-        eventEditButton.classList.add("btn-info");
-        eventEditButton.addEventListener("click", function(){
+        if(eventObj.userId === Number(userID)){
+            eachEventContainer.classList.add("myEvent");
+            const eventButtonGroup = eachEventContainer.appendChild(HTMLFactory.createElementWithText("div", undefined, `eventCard-buttonGroup--${eventObj.id}`));
+            eventButtonGroup.classList.add("btn-group");
+            const eventEditButton = eventButtonGroup.appendChild(HTMLFactory.createElementWithText("button", "Edit Event", `eventCard-editButton--${eventObj.id}`));
+            eventEditButton.classList.add("btn");
+            eventEditButton.classList.add("btn-sm");
+            eventEditButton.classList.add("btn-info");
+            eventEditButton.addEventListener("click", function(){
             const nameToCopy = event.target.parentNode.parentNode.firstChild;
             const dateToCopy = nameToCopy.nextSibling;
             const locationToCopy = dateToCopy.nextSibling;
-            ;
             let id = Number(event.target.id.split("--")[1])
             HTMLFactory.clearContainer(eventSection);
             eventSection.appendChild(editEventForm.editEventFormBuilder(nameToCopy.textContent, dateToCopy.textContent, locationToCopy.textContent, id));
@@ -44,23 +47,35 @@ const eventHTML = {
         eventDeleteButton.classList.add("btn-sm");
         eventDeleteButton.classList.add("btn-danger");
         eventDeleteButton.addEventListener("click", handlersForEvents.deleteEvent);
+        } else if (eventObj.userId !== Number(userID)) {
+            eachEventContainer.classList.add("friendEvent");
+        }
     },
     // listEventsToDom takes the array of events and calls the htmlForEachEvent function for each event, and appends each one to the DOM.
     listEventsToDom: (eventsArray) => {
         const eventSection = document.querySelector("#events-section");
-        eventSection.appendChild(eventHTML.buildEventCard());
-        eventsArray.forEach(event => {
+        if(!eventSection.firstChild){
+            eventSection.appendChild(eventHTML.buildEventCard());
+            eventsArray.forEach(event => {
             eventHTML.HTMLforEachEvent(event);
-        })
-        const eventCard = document.querySelector("#eventCard-card");
-        const cardFooter = eventCard.appendChild(HTMLFactory.createElementWithText("div", undefined, "eventCard-footer"));
-        cardFooter.classList.add("card-footer");
-        const newEventButton = cardFooter.appendChild(HTMLFactory.createElementWithText("button", "Create New Event", "eventCard-createButton"));
-        newEventButton.classList.add("btn");
-        newEventButton.classList.add("btn-block");
-        newEventButton.classList.add("btn-primary");
-        newEventButton.addEventListener("click", handlersForEvents.createNewEventHandler);
-        return eventCard;
+            })
+            let eventCard = document.querySelector("#eventCard-card");
+            const cardFooter = eventCard.appendChild(HTMLFactory.createElementWithText("div", undefined, "eventCard-footer"));
+            cardFooter.classList.add("card-footer");
+            const newEventButton = cardFooter.appendChild(HTMLFactory.createElementWithText("button", "Create New Event", "eventCard-createButton"));
+            newEventButton.classList.add("btn");
+            newEventButton.classList.add("btn-block");
+            newEventButton.classList.add("btn-primary");
+            newEventButton.addEventListener("click", handlersForEvents.createNewEventHandler);
+            return eventCard;
+        } else {
+            let eventCardBody = document.querySelector("#eventCard-body");
+            eventsArray.forEach(event => {
+                eventHTML.HTMLforEachEvent(event);
+            })
+            return eventCardBody;
+        }
+        
     }
 }
 
