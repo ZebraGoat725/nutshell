a41c121afef2f1fdea4254e6bc5779d3ef2ec632
Billy Mathison <wlmathison@gmail.com>
2019-04-07 17:14:42 -0500

    adding bootstrap card styling to friends section and adding additional jpeg to images gallery

--- src/scripts/friendScripts/buildFriendsSection.js
+++ src/scripts/friendScripts/buildFriendsSection.js
@@ -5,37 +5,49 @@ const buildFriendsSection = {
     // Function to create div container for user information
     createTopDiv(userName, userEmail, userImage) {
         const divSideBar = HTMLFactory.createElementWithText("div");
-        divSideBar.classList = "sidenav";
-        divSideBar.appendChild(HTMLFactory.createElementWithText("h1", "Friends"));
-        divSideBar.appendChild(HTMLFactory.createElementWithText("div"));
+        divSideBar.classList = "card"
+        let headerText = HTMLFactory.createElementWithText("h1", "Friends")
+        headerText.classList = "card-header";
+        divSideBar.appendChild(headerText);
+        let bodyDiv = HTMLFactory.createElementWithText("div");
+        bodyDiv.classList = "card-body";
         let userPic = HTMLFactory.createElementWithText("IMG");
         userPic.setAttribute("src", userImage)
-        divSideBar.appendChild(userPic);
-        divSideBar.appendChild(HTMLFactory.createElementWithText("h3", userName, "current-user-name"));
-        divSideBar.appendChild(HTMLFactory.createElementWithText("h3", userEmail));
-        divSideBar.appendChild(HTMLFactory.createElementWithText("hr"));
+        bodyDiv.appendChild(userPic);
+        bodyDiv.appendChild(HTMLFactory.createElementWithText("h3", userName, "current-user-name"));
+        bodyDiv.appendChild(HTMLFactory.createElementWithText("h3", userEmail));
+        divSideBar.appendChild(bodyDiv)
         return divSideBar;
     },
     // Function to create individual div containers for each friend
     createFriendDiv(friendName, friendEmail, friendImage, friendId) {
         const friendDiv = HTMLFactory.createElementWithText("div");
+        friendDiv.classList = "card";
+        let bodyDiv = HTMLFactory.createElementWithText("div");
+        bodyDiv.classList = "card-body"
         let userPic = HTMLFactory.createElementWithText("IMG");
         userPic.setAttribute("src", friendImage)
-        friendDiv.appendChild(userPic);
-        friendDiv.appendChild(HTMLFactory.createElementWithText("h4", friendName));
-        friendDiv.appendChild(HTMLFactory.createElementWithText("h4", friendEmail));
+        bodyDiv.appendChild(userPic);
+        bodyDiv.appendChild(HTMLFactory.createElementWithText("h4", friendName));
+        bodyDiv.appendChild(HTMLFactory.createElementWithText("h4", friendEmail));
         let deleteButton = HTMLFactory.createElementWithText("button", "Delete Friend", `delete-friend--${friendId}`);
+        deleteButton.classList = "btn btn-sm btn-danger";
         deleteButton.addEventListener("click", friendEventHandler.deleteFriend)
-        friendDiv.appendChild(deleteButton);
-        friendDiv.appendChild(HTMLFactory.createElementWithText("hr"));
+        bodyDiv.appendChild(deleteButton);
+        friendDiv.appendChild(bodyDiv);
         return friendDiv;
     },
     // Function to create div container with input to save new friend
     createSaveFriendDiv() {
         const saveFriendDiv = HTMLFactory.createElementWithText("div");
+        saveFriendDiv.classList = "card";
+        const saveFriendFooterDiv = HTMLFactory.createElementWithText("div");
+        saveFriendFooterDiv.classList = "card-footer";
         let addFriendButton = HTMLFactory.createElementWithText("button", "Add a friend");
+        addFriendButton.classList = "btn btn-primary";
         addFriendButton.addEventListener("click", friendEventHandler.addFriend)
-        saveFriendDiv.appendChild(addFriendButton);
+        saveFriendFooterDiv.appendChild(addFriendButton);
+        saveFriendDiv.appendChild(saveFriendFooterDiv);
         return saveFriendDiv;
     },
     // Factory function to create a friend object to be posted to friends resource
