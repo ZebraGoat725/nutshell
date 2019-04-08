79dc2013de9157703717b34685312afe373005c2
Chris Morgan <bluemorgan2323@gmail.com>
2019-04-08 09:27:37 -0500

    The registration form now checks to see if the username and email have been taken. database.json.example dates have been updated.

--- src/scripts/loginScripts/loginHandler.js
+++ src/scripts/loginScripts/loginHandler.js
@@ -3,12 +3,21 @@ import registerForm from "./registerForm"
 import API from "./../apiManager.js"
 import loadPage from "./loadPage"
 
-
 //createNewUserObj is meant to be a factory function that is used by the post new user method.
 const createNewUserObj = (userName, userEmail) => {
     return {
         userName: userName,
-        userEmail: userEmail
+        email: userEmail,
+        image: "images/racoon.jpg"
+    }
+}
+
+const checkUserName = (userArray, searchBy) => {
+    let isMatch = userArray.find(user => user.userName === searchBy);
+    if(!isMatch){
+        return false;
+    } else {
+        return true;
     }
 }
 
@@ -43,10 +52,31 @@ const loginHandler = {
     // Submit function creates an object with the user input and posts the new object into the database.
     submit() {
         const section = document.querySelector("#login-section");
-        const newNameInput = document.querySelector("#registerName-input").value;
-        const newEmailInput = document.querySelector("#registerEmail-input").value;
-        API.postCreateUser(createNewUserObj(newNameInput, newEmailInput));
-        HTMLFactory.clearContainer(section);
+        const newNameInput = document.querySelector("#registerName-input");
+        const newEmailInput = document.querySelector("#registerEmail-input");
+        let matches = [];
+        if(newNameInput.value === "") {
+            return alert("You left the Username input blank");
+        }else if(newEmailInput.value === "") {
+            return alert("You left the Email input blank");
+        }
+        API.getUsers().then(users => users.forEach(user => {
+            if(newNameInput.value.toLowerCase() === user.userName.toLowerCase()){
+                alert("This username has been taken.")
+                return matches.push(user.userName)
+            }
+        }))
+        API.getUsers().then(users => users.forEach(user => {
+            if(newEmailInput.value.toLowerCase() === user.email.toLowerCase()){
+                alert("This email is already registered.")
+                return matches.push(user.email);
+            }
+        })).then(() => {
+            if(matches.length === 0){
+            API.postCreateUser(createNewUserObj(newNameInput.value, newEmailInput.value));
+            HTMLFactory.clearContainer(section);
+        }
+        })
     }
 };
 
