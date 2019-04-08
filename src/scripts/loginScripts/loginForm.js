9b92d60377116733bb2f8337a561f977e1d81cd1
Chris Morgan <bluemorgan2323@gmail.com>
2019-04-08 14:37:06 -0500

    Added bootstrap classes to login and register forms. Fixed registration validation.

--- src/scripts/loginScripts/loginForm.js
+++ src/scripts/loginScripts/loginForm.js
@@ -8,34 +8,40 @@ const loginForm = {
 
         //create elements for login form
         let outerLoginDiv = HTMLFactory.createElementWithText("div", "", "outerLoginDiv")
-
-        let welcomeHeader = HTMLFactory.createElementWithText("h1", "Welcome yo!!!!")
-
-        let formElement = HTMLFactory.createElementWithText("form", "")
-
-        let loginDiv = HTMLFactory.createElementWithText("div", "")
-
+        outerLoginDiv.classList.add("card");
+        let welcomeHeader = outerLoginDiv.appendChild(HTMLFactory.createElementWithText("h1", "Welcome to Nutshell!"))
+        welcomeHeader.classList.add("card-header");
+        let loginBody = outerLoginDiv.appendChild(HTMLFactory.createElementWithText("div"));
+        loginBody.classList.add("card-body");
+        loginBody.classList.add("bg-light")
+        let formElement = loginBody.appendChild(HTMLFactory.createElementWithText("form"))
+        let loginDiv = formElement.appendChild(HTMLFactory.createElementWithText("div"));
+        loginDiv.classList.add("form-group")
         let userNameLabel = HTMLFactory.createElementWithText("label", "Username: ")
 
         let userNameInput = HTMLFactory.createElementWithText("input", "", "userNameInput")
-
+        userNameInput.classList.add("form-control")
         userNameInput.placeholder = "Username"
 
-        let emailDiv = HTMLFactory.createElementWithText("div", "")
-
+        let emailDiv = formElement.appendChild(HTMLFactory.createElementWithText("div", ""))
+        emailDiv.classList.add("form-group");
         let emailLabel = HTMLFactory.createElementWithText("label", "Email: ")
 
         let emailInput = HTMLFactory.createElementWithText("input", "", "emailInput")
-
+        emailInput.classList.add("form-control");
         emailInput.placeholder = "example@example.com"
 
         //create login and register button
-        let loginButton = HTMLFactory.createElementWithText("button", "Login", "loginButton")
-
+        let loginButtonGroup = formElement.appendChild(HTMLFactory.createElementWithText("div"))
+        loginButtonGroup.classList.add("btn-group")
+        let loginButton = loginButtonGroup.appendChild(HTMLFactory.createElementWithText("button", "Login", "loginButton"))
+        loginButton.classList.add("btn");
+        loginButton.classList.add("btn-primary")
         loginButton.type = "button"
 
-        let registerButton = HTMLFactory.createElementWithText("button", "Register", "registerButton")
-
+        let registerButton = loginButtonGroup.appendChild(HTMLFactory.createElementWithText("button", "Register", "registerButton"))
+        registerButton.classList.add("btn");
+        registerButton.classList.add("btn-secondary")
         registerButton.type = "button"
 
         loginButton.addEventListener("click", loginHandler.login)
@@ -47,12 +53,6 @@ const loginForm = {
         loginDiv.appendChild(userNameInput)
         emailDiv.appendChild(emailLabel)
         emailDiv.appendChild(emailInput)
-        formElement.appendChild(loginDiv)
-        formElement.appendChild(emailDiv)
-        formElement.appendChild(loginButton)
-        formElement.appendChild(registerButton)
-        outerLoginDiv.appendChild(welcomeHeader)
-        outerLoginDiv.appendChild(formElement)
         loginFragment.appendChild(outerLoginDiv)
 
 
